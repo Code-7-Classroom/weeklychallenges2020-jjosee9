5568fe73a8ecff7958b64c8e967b46a827eeb2da
Jose Hernandez <r2h@Administrators-MacBook-Pro.local>
2020-03-30 11:16:29 -0400

    checkpointarrays

---
+++ checkpointArrays/script.js
@@ -0,0 +1,8 @@
+//Array methods Pop(), push(), shift()
+
+var drinks = ["Water", "Coffee", "Tea"];
+var foods = ["Apple", "Sandwich", "Chips"];
+
+var meals = drinks.concat(foods)
+
+console.log([meals]);
