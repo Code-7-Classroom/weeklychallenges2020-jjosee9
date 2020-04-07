98cbd855d787b5a47f54394627eb25102ca27e2b
Jose Hernandez <r2h@adminisatorsmbp.home>
2020-04-06 23:12:24 -0400

    updated

--- 08-javascript/veryHard08/script.js
+++ 08-javascript/veryHard08/script.js
@@ -1,11 +1,7 @@
 //1. Create a Person constructor that has three properties: name, job, and age.
 //2. Give the Person an 'exercise' method that console logs whatever you want, e.g. "Running is fun! - said no one ever".
 //3. Give the Person a 'fetchJob' method that console logs the person's name and job, e.g. "Brad is a    back-end developer".
-//  var john = {
-//      name: 'John',
-//      exercise: 'running',
-//      job: 'teacher'
-//  };
+
 // 4. Create a Programmer constructor that inherits all the members from Person with 
 //an additional 'languages' property that is passed in and a busy property that is NOT
  //passed in and is set to true by default.
@@ -30,14 +26,13 @@ var Person = function (name, job, age, ) {
      }
 }
 
+Person.prototype.
+
 var john = new Person ()//running);
 john.activity();
 
 var profession = new Person()
 john.profession();
-
-var programmer = new Person('languages');
-programmer.constructor === Person // => true
 
 
 
