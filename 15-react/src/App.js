6fcfab16d65d3557a1a21391594a71deff00dc60
Jose Hernandez <r2h@AdminisatorsMBP.attlocal.net>
2020-05-19 17:18:20 -0400

    very easy

---
+++ 15-react/src/App.js
@@ -0,0 +1,43 @@
+// import React from 'react';
+// import logo from './logo.svg';
+// import './App.css';
+import React, { Component } from 'react'
+
+class App extends Component {
+  render(){
+    return (
+      <div>
+        <h1>Jose Hernandez</h1>
+        <p>9804283678</p>
+        <p>11/09/1994</p>
+      </div>
+    )
+  }
+}
+
+
+
+
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+export default App;
